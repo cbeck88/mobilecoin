966f5e00d0d55127d77f53f04f4799ee3cb02eaf
Chris Beck <beck.ct@gmail.com>
2022-02-02 17:12:35 -0700

    fix util telemetry build (wierd rebase)

--- util/telemetry/src/lib.rs
+++ util/telemetry/src/lib.rs
@@ -9,6 +9,7 @@ pub use opentelemetry::{
 
 use opentelemetry::{
     global::{tracer_provider, BoxedTracer},
+    sdk::{trace::Config, Resource},
     trace::{SpanBuilder, TraceId, TracerProvider},
 };
 use std::borrow::Cow;
